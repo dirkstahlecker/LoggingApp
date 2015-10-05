9ff15b6d505da7fbd148bc0bfe614406ab2eb984
Dirk <stahdirk@mit.edu>
2015-10-05 01:32:31 -0400

    changed to version 0.2 for release to Ari for classroom

--- src/model/Constants.java
+++ src/model/Constants.java
@@ -6,7 +6,7 @@ import javafx.util.Duration;
 
 public class Constants {
 	public static boolean DEBUG = false;
-	public static final double volumeGain = 0.1;
+	public static final double volumeGain = 0.2;
 	public static Duration rewindGain = new Duration(5000); //5 seconds
 	public static Duration fastforwardGain = new Duration(5000);
 	public static Double playbackRate = new Double(1.0);
